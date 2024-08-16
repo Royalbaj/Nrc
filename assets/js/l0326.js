ffe18076cfd003aa5f61af8d427956c885c6ea8d
Janak Panthee <janakpanthi78@gmail.com>
2024-08-16 19:15:36 +0545

    .

--- assets/js/l0326.js
+++ assets/js/l0326.js
@@ -1,33 +1,40 @@
-// scripts.js
-
 function saveContact() {
     try {
+        
         const vCardData = `
-            BEGIN:VCARD
-            VERSION:3.0
-            N:;Krishna;Bhandari;;
-            FN:Krishna Bhandari
-            TEL:+977-9857053661
-            EMAIL:krishnabhandari.info@gmail.com
-            URL:https://www.linkedin.com/in/krishna-bhandari-ab1b60158?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app
-            TITLE:Managing Director at Silicon Education Butwal
-            ORG:Proactive Path Education Network
-            END:VCARD
+BEGIN:VCARD
+VERSION:3.0
+N:;Krishna;Bhandari;;;
+FN:Krishna Bhandari
+TEL:+977-9857053661
+EMAIL:krishnabhandari.info@gmail.com
+URL:https://www.linkedin.com/in/krishna-bhandari-ab1b60158?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app
+TITLE:Managing Director at Silicon Education Butwal
+ORG:Proactive Path Education Network
+END:VCARD
         `;
 
-        const blob = new Blob([vCardData], { type: 'text/x-vcard' });
-        const url = window.URL.createObjectURL(blob);
+        // Create a Blob with the vCard data
+        const blob = new Blob([vCardData], { type: 'text/vcard;charset=utf-8' });
+        
+        // Create a URL for the Blob
+        const url = URL.createObjectURL(blob);
+
+        // Create an anchor element and trigger a download
         const a = document.createElement('a');
         a.href = url;
-        a.download = 'KrishnaBhandari.vcf'; // Ensure the correct filename
+        a.download = 'KrishnaBhandari.vcf'; 
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
+
+        // Clean up the Blob URL
+        URL.revokeObjectURL(url);
     } catch (error) {
         console.error('Error creating vCard:', error);
     }
 }
+
 
 function generateQR() {
     const currentURL = window.location.href;
